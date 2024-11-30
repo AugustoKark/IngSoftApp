0c83c95ae13a03841d205b61703ea270cd0098e4
AugustoKark <augustokark@hotmail.com>
2024-11-30 19:38:54 -0300

    Ionic App

---
+++ rentalcar-app/src/views/HomePage.vue
@@ -0,0 +1,151 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button>Home</ion-button>
+          <ion-button>Mis alquileres</ion-button>
+          <ion-button>Login</ion-button>
+        </ion-buttons>
+        <ion-title>Rental Car</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <div class="welcome-hero-txt">
+        <h2>Alquila el auto de tus sueños a un buen precio</h2>
+        <p>
+          En RENTALCAR tenemos los mejores autos para ti. Nosotros nos encargamos de que tengas la mejor experiencia al alquilar un auto.
+        </p>
+      </div>
+
+      <div class="featured-cars-content">
+        <div class="row">
+          <div v-for="car in cars" :key="car.id" class="col-lg-3 col-md-4 col-sm-6">
+            <div class="single-featured-cars">
+              <div class="featured-img-box">
+                <div class="featured-cars-img">
+                  <!-- <img :src="require(`@/assets/images/featured-cars/${car.img}.png`)" alt="cars"> -->
+                </div>
+                <div class="featured-model-info">
+                  <p>
+                    model: {{ new Date().getFullYear() }}
+                    <span class="featured-mi-span"> {{car.km}} Km</span>
+                    <span class="featured-hp-span"> {{car.hp}}HP</span>
+                    {{car.transmision}}
+                  </p>
+                </div>
+              </div>
+              <div class="featured-cars-txt">
+                <h2><a href="#">{{car.modelo}}</a></h2>
+                <h3>${{car.precio}} <ion-button class="rent-button" >Alquilar</ion-button></h3>
+                <p>{{car.descripcion}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+import { ref, onMounted } from 'vue';
+
+const isLoggedIn = ref(false);
+const cars = ref([
+  {
+    id: 1,
+    modelo: 'Toyota Corolla',
+    precio: 100,
+    km: 10000,
+    hp: 200,
+    transmision: 'Automatico',
+    img: 'car1',
+    descripcion: 'El Toyota Corolla es un automóvil del segmento C producido por el fabricante japonés Toyota. Se trata de uno de los automóviles más vendidos de la historia.'
+  },
+  {
+    id: 2,
+    modelo: 'Honda Civic',
+    precio: 120,
+    km: 8000,
+    hp: 180,
+    transmision: 'Automatico',
+    img: 'car2',
+    descripcion: 'El Honda Civic es un automóvil del segmento C producido por el fabricante japonés Honda. Es un automóvil muy popular en todo el mundo.'
+  },
+  {
+    id: 3,
+    modelo: 'Nissan Sentra',
+    precio: 90,
+    km: 12000,
+    hp: 150,
+    transmision: 'Automatico',
+    img: 'car3',
+    descripcion: 'El Nissan Sentra es un automóvil del segmento C producido por el fabricante japonés Nissan. Es un automóvil muy popular en todo el mundo.'
+  },
+  {
+    id: 4,
+    modelo: 'Chevrolet Cruze',
+    precio: 110,
+    km: 9000,
+    hp: 170,
+    transmision: 'Automatico',
+    img: 'car4',
+    descripcion: 'El Chevrolet Cruze es un automóvil del segmento C producido por el fabricante estadounidense Chevrolet. Es un automóvil muy popular en todo el mundo.'
+  }
+]);
+
+
+
+
+</script>
+
+<style scoped>
+.welcome-hero-txt {
+  position: relative;
+  background-image: url('@/assets/images/welcome-banner.jpg'); /* Asegúrate de que la ruta sea correcta */
+  text-align: center;
+  background-size: cover;
+  background-position: center;
+  z-index: 1;
+  width: 100%;
+  height: 40vh; /* Ajusta la altura según sea necesario */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white; /* Asegúrate de que el texto sea visible sobre la imagen */
+}
+
+.featured-cars-content {
+  padding: 20px;
+  background-color: white; /* Asegúrate de que el fondo sea visible */
+}
+
+.featured-cars-img img {
+  width: 100%;
+  height: auto;
+}
+
+.single-featured-cars {
+  margin-bottom: 20px;
+}
+
+.featured-model-info p {
+  margin: 0;
+}
+
+.featured-cars-txt h2 {
+  margin: 0;
+}
+
+.featured-cars-txt h3 {
+  margin: 0;
+}
+
+.rent-button {
+  margin-left: 10px;
+}
+</style>
---
+++ rentalcar-app/src/views/Login.vue
@@ -0,0 +1,417 @@
+<template>
+    <div class="container">
+      <div class="wrapper">
+        <div class="title-text">
+          <div class="title login">Login</div>
+          <div class="title signup">Register</div>
+        </div>
+        <div class="form-container">
+          <div class="slide-controls">
+            <input type="radio" name="slide" id="login" checked>
+            <input type="radio" name="slide" id="signup">
+            <label for="login" class="slide login">Login</label>
+            <label for="signup" class="slide signup">Register</label>
+            <div class="slider-tab"></div>
+          </div>
+          <div class="form-inner">
+            <form @submit.prevent="login" class="login">
+              <div class="field">
+                <input type="text" v-model="username" placeholder="Username" required>
+              </div>
+              <div class="field">
+                <input type="password" v-model="password" placeholder="Password" required>
+              </div>
+              <div class="pass-link">
+                <a href="#" @click.prevent="redirectToRecoveryPassword">Forgot password?</a>
+              </div>
+              <div class="error-message">{{ loginError }}</div>
+              <div class="field btn">
+                <div class="btn-layer"></div>
+                <input type="submit" value="Login">
+              </div>
+              <div class="signup-link">
+                Not a member? <a href="">Register now</a>
+              </div>
+            </form>
+            <form @submit.prevent="register" class="signup">
+              <div class="field">
+                <input type="text" v-model="registerUsername" placeholder="Username" required>
+              </div>
+              <div class="field">
+                <input type="text" v-model="registerEmail" placeholder="Email Address" required>
+              </div>
+              <div class="field">
+                <input type="password" v-model="registerPassword" placeholder="Password" required>
+              </div>
+              <div class="field">
+                <input type="password" v-model="registerConfirmPassword" placeholder="Confirm password" required>
+              </div>
+              <div class="error-message">{{ registerError }}</div>
+              <div class="field btn">
+                <div class="btn-layer"></div>
+                <input type="submit" value="Signup">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  
+  const username = ref('');
+  const password = ref('');
+  const registerUsername = ref('');
+  const registerEmail = ref('');
+  const registerPassword = ref('');
+  const registerConfirmPassword = ref('');
+  const loginError = ref('');
+  const registerError = ref('');
+  
+  async function login() {
+    try {
+      const response = await fetch('http://localhost:8080/api/authenticate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value
+        })
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        loginError.value = '';
+        localStorage.setItem('jwt', data.jwt);
+        localStorage.setItem('userId', data.userId);
+        router.push('/home');
+      } else {
+        loginError.value = 'Nombre de usuario o contraseña incorrectos.';
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+      loginError.value = 'Error de red. Por favor, intenta nuevamente.';
+    }
+  }
+  
+  async function register() {
+    if (registerPassword.value !== registerConfirmPassword.value) {
+      registerError.value = 'Las contraseñas no coinciden.';
+      return;
+    }
+  
+    const userData = {
+      login: registerUsername.value,
+      email: registerEmail.value,
+      password: registerPassword.value,
+      langKey: 'es'
+    };
+  
+    try {
+      const response = await fetch('http://localhost:8080/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      });
+  
+      if (response.status === 201) {
+        registerError.value = '';
+        registerUsername.value = '';
+        registerEmail.value = '';
+        registerPassword.value = '';
+        registerConfirmPassword.value = '';
+        switchToLogin();
+      } else {
+        const data = await response.json();
+        if (data.message === 'error.userexists') {
+          registerError.value = 'El usuario ya existe.';
+        } else if (data.message === 'error.emailexists') {
+          registerError.value = 'El correo electrónico ya está en uso.';
+        } else {
+          registerError.value = data.message || 'El registro ha fallado. Por favor, intenta nuevamente.';
+        }
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+      registerError.value = 'Error de red. Por favor, intenta nuevamente.';
+    }
+  }
+  
+  function switchToLogin() {
+    const loginForm = document.querySelector("form.login");
+    const loginText = document.querySelector(".title-text .login");
+    const loginBtn = document.querySelector("label.login");
+  
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+    loginBtn.click();
+  }
+  
+  function redirectToRecoveryPassword() {
+    router.push({ name: "recovery-password" });
+  }
+  
+  onMounted(() => {
+    const loginText = document.querySelector(".title-text .login");
+    const loginForm = document.querySelector("form.login");
+    const loginBtn = document.querySelector("label.login");
+    const signupBtn = document.querySelector("label.signup");
+    const signupLink = document.querySelector("form .signup-link a");
+  
+    signupBtn.onclick = () => {
+      loginForm.style.marginLeft = "-50%";
+      loginText.style.marginLeft = "-50%";
+    };
+  
+    loginBtn.onclick = () => {
+      loginForm.style.marginLeft = "0%";
+      loginText.style.marginLeft = "0%";
+    };
+  
+    signupLink.onclick = () => {
+      signupBtn.click();
+      return false;
+    };
+  });
+  </script>
+  
+  <style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  html, body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    background: -webkit-linear-gradient(left, #455eb2, #4298fa);
+  }
+  
+  ::selection {
+    background: #427cfa;
+    color: #fff;
+  }
+  
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+  
+  .wrapper {
+    overflow: hidden;
+    max-width: 390px;
+    background: #fff;
+    padding: 30px;
+    border-radius: 5px;
+    box-shadow: 0px 15px 20px rgba(0,0,0,0.1);
+  }
+  
+  .wrapper .title-text {
+    display: flex;
+    width: 200%;
+  }
+  
+  .wrapper .title {
+    width: 50%;
+    font-size: 35px;
+    font-weight: 600;
+    text-align: center;
+    transition: all 0.6s cubic-bezier(0.68,-0.55,0.265,1.55);
+  }
+  
+  .wrapper .slide-controls {
+    position: relative;
+    display: flex;
+    height: 50px;
+    width: 100%;
+    overflow: hidden;
+    margin: 30px 0 10px 0;
+    justify-content: space-between;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+  }
+  
+  .slide-controls .slide {
+    height: 100%;
+    width: 100%;
+    color: #fff;
+    font-size: 18px;
+    font-weight: 500;
+    text-align: center;
+    line-height: 48px;
+    cursor: pointer;
+    z-index: 1;
+    transition: all 0.6s ease;
+  }
+  
+  .slide-controls label.signup {
+    color: #000;
+  }
+  
+  .slide-controls .slider-tab {
+    position: absolute;
+    height: 100%;
+    width: 50%;
+    left: 0;
+    z-index: 0;
+    border-radius: 5px;
+    background: -webkit-linear-gradient(left, #456bb2, #4282fa);
+    transition: all 0.6s cubic-bezier(0.68,-0.55,0.265,1.55);
+  }
+  
+  input[type="radio"] {
+    display: none;
+  }
+  
+  #signup:checked ~ .slider-tab {
+    left: 50%;
+  }
+  
+  #signup:checked ~ label.signup {
+    color: #fff;
+    cursor: default;
+    user-select: none;
+  }
+  
+  #signup:checked ~ label.login {
+    color: #000;
+  }
+  
+  #login:checked ~ label.signup {
+    color: #000;
+  }
+  
+  #login:checked ~ label.login {
+    cursor: default;
+    user-select: none;
+  }
+  
+  .wrapper .form-container {
+    width: 100%;
+    overflow: hidden;
+  }
+  
+  .form-container .form-inner {
+    display: flex;
+    width: 200%;
+  }
+  
+  .form-container .form-inner form {
+    width: 50%;
+    transition: all 0.6s cubic-bezier(0.68,-0.55,0.265,1.55);
+  }
+  
+  .form-inner form .field {
+    height: 50px;
+    width: 100%;
+    margin-top: 20px;
+  }
+  
+  .form-inner form .field input {
+    height: 100%;
+    width: 100%;
+    outline: none;
+    padding-left: 15px;
+    border-radius: 5px;
+    border: 1px solid lightgrey;
+    border-bottom-width: 2px;
+    font-size: 17px;
+    transition: all 0.3s ease;
+  }
+  
+  .form-inner form .field input:focus {
+    border-color: #8b83fc;
+  }
+  
+  .form-inner form .field input::placeholder {
+    color: #999;
+    transition: all 0.3s ease;
+  }
+  
+  form .field input:focus::placeholder {
+    color: #b3b3b3;
+  }
+  
+  .form-inner form .pass-link {
+    margin-top: 5px;
+  }
+  
+  .form-inner form .signup-link {
+    text-align: center;
+    margin-top: 30px;
+  }
+  
+  .form-inner form .pass-link a,
+  .form-inner form .signup-link a {
+    color: #4282fa;
+    text-decoration: none;
+  }
+  
+  .form-inner form .pass-link a:hover,
+  .form-inner form .signup-link a:hover {
+    text-decoration: underline;
+  }
+  
+  form .btn {
+    height: 50px;
+    width: 100%;
+    border-radius: 5px;
+    position: relative;
+    overflow: hidden;
+  }
+  
+  form .btn .btn-layer {
+    height: 100%;
+    width: 300%;
+    position: absolute;
+    left: -100%;
+    background: -webkit-linear-gradient(right, #456bb2, #427cfa, #455eb2, #42a1fa);
+    border-radius: 5px;
+    transition: all 0.4s ease;
+  }
+  
+  form .btn:hover .btn-layer {
+    left: 0;
+  }
+  
+  form .btn input[type="submit"] {
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+    position: relative;
+    background: none;
+    border: none;
+    color: #fff;
+    padding-left: 0;
+    border-radius: 5px;
+    font-size: 20px;
+    font-weight: 500;
+    cursor: pointer;
+  }
+  
+  .error-message {
+    color: red;
+    margin-top: 10px;
+    text-align: center;
+  }
+  </style>
